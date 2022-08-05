36597dd5aa1552686ae317cb88e1cf2924011cab
JAntonioFloresS <jflores40075@ufide.ac.cr>
2022-08-04 20:49:38 -0600

    Historia de usuario 6, cambio de idioma

---
+++ app/src/main/java/com/peluqueria/data/CitaDatabase.kt
@@ -0,0 +1,35 @@
+package com.peluqueria.data
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.peluqueria.model.Cita
+
+
+//Este codigo es para contruir la base de datos, si no existe la contruye y si existe devuelve la referencia a la base de datos.
+@Database(entities = [Cita::class], version = 1, exportSchema = false)
+abstract class CitaDatabase: RoomDatabase() {
+    abstract fun citaDao():CitaDao
+
+    companion object{
+        @Volatile
+        private var INSTANCE: CitaDatabase?=null
+
+        fun getDatabase(context: android.content.Context):CitaDatabase{
+            val tempInstance= INSTANCE
+            if(tempInstance!=null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CitaDatabase::class.java,
+                    "cita_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/peluqueria/ui/CitaAdapter.kt
@@ -0,0 +1,48 @@
+package com.peluqueria.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.peluqueria.databinding.CitaFilaBinding
+import com.peluqueria.model.Cita
+
+class CitaAdapter : RecyclerView.Adapter<CitaAdapter.CitaViewHolder>() {
+
+    //Se crea una lista para almacenar las citas
+    private var listaCitas = emptyList<Cita>()
+
+    inner class CitaViewHolder(private val itemBinding: CitaFilaBinding) :
+            RecyclerView.ViewHolder(itemBinding.root){
+    fun bind(cita: Cita) {
+        itemBinding.tvNombre.text = cita.telefono
+        itemBinding.tvCorreo.text = cita.telefono
+        itemBinding.tvTelefono.text = cita.telefono
+        itemBinding.tvFecha.text = cita.telefono
+        itemBinding.tvHora.text = cita.telefono
+        itemBinding.tvEstilista.text = cita.telefono
+        itemBinding.tvMonto.text = cita.telefono
+
+    }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CitaViewHolder {
+       val itemBinding = CitaFilaBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return CitaViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: CitaViewHolder, position: Int) {
+        val citaActual = listaCitas[position]
+        holder.bind(citaActual)
+    }
+
+    override fun getItemCount(): Int {
+        return listaCitas.size
+    }
+    fun setData (citas: List<Cita>){
+        this.listaCitas = citas
+        notifyDataSetChanged()
+    //Hace que se redibuje la lista
+
+
+    }
+}
